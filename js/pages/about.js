8bb572f4951529670bc98ad23095c937148affdb
runegrid <brgui86@gmail.com>
2026-05-23 18:50:35 -0300

    third

--- js/pages/about.js
+++ js/pages/about.js
@@ -6,7 +6,7 @@ function about() {
     <div class="page-header">
       <h1>${config.website.name}</h1>
     </div>
-    <p>Guias & Projetos para Desenvolvedores. Front-end com HTML, CSS e JavaScript — conteúdo técnico sobre desenvolvimento front-end com foco em fundamentos, performance e boas práticas.</p>
+    <p>Guias & Projetos para Desenvolvedores. HTML, CSS e JavaScript — conteúdo técnico sobre desenvolvimento front-end com foco em fundamentos, performance e boas práticas.</p>
     <p>${config.website.description}</p>
   `);
 }
