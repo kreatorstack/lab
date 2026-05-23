07b02f927afb994df31c033b2a3cb8a6f2e2d8cd
runegrid <brgui86@gmail.com>
2026-05-22 21:47:57 -0300

    newstest

--- js/pages/contact.js
+++ js/pages/contact.js
@@ -7,7 +7,7 @@ function contact() {
     </div>
     <ul class="contact-links">
       <li><a href="mailto:topverbs@gmail.com" target="_blank">topverbs@gmail.com</a></li>
-      <li><a href="https://linkedin.com/in/gribeirodev" target="_blank">linkedin.com/in/gribeirodev</a></li>
+      <li><a href="https://linkedin.com/in/grfront" target="_blank">linkedin.com/in/grfront</a></li>
       <li><a href="https://github.com/grfront" target="_blank">github.com/grfront</a></li>
       <li><a href="https://instagram.com/front.stack" target="_blank">instagram.com/front.stack</a></li>
     </ul>
